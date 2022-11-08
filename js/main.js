bc8d3001574964fc8b0c345a272f34f66f0ecaa4
Ilya_kalchanka <ilya06041994@gmail.com>
2022-11-09 00:33:25 +0300

    commit modal activate modal form

--- js/main.js
+++ js/main.js
@@ -11,9 +11,14 @@ var product = document.getElementById("header__prod"),
     influence = document.getElementById("header__influence"),
     infBtn = document.getElementById("influence-btn"),
     prodBtn = document.getElementById("prod-btn"),
-    closeBtn = document.getElementById('close-btn'),
-    overlay = document.getElementById('overlay'),
-    modal = document.getElementById('modal');
+    closeBtn = document.getElementById("close-btn"),
+    overlay = document.getElementById("overlay"),
+    modal = document.getElementById("modal"),
+    choiseProdBtn = document.getElementById("choise__prod-btn"),
+    formOne = document.getElementById("modal-window__form-one"),
+    formTwo = document.getElementById("modal-window__form-two"),
+    choiseInfBtn = document.getElementById("choise__inf-btn");
+
 
 product.addEventListener("mouseover", () => {
     prodBtn.classList.add("active");
@@ -28,14 +33,42 @@ influence.addEventListener("mouseout", () => {
     infBtn.classList.remove("active");
 });
 infBtn.addEventListener('click', () => {
-    modal.classList.add('active');
+    modal.classList.add("active");
+    choiseInfBtn.classList.add("hover");
+    formTwo.classList.add("active");
 });
 prodBtn.addEventListener('click', () => {
-    modal.classList.add('active');
+    modal.classList.add("active");
+    choiseProdBtn.classList.add("hover");
+    formOne.classList.add("active");
 });
 closeBtn.addEventListener('click', () => {
-    modal.classList.remove('active');
+    modal.classList.remove("active");
+    choiseProdBtn.classList.remove("hover");
+    choiseInfBtn.classList.remove("hover");
+    formOne.classList.remove("active");
+    formTwo.classList.remove("active");
 });
 overlay.addEventListener('click', () => {
-    modal.classList.remove('active');
+    modal.classList.remove("active");
+    choiseInfBtn.classList.remove("hover");
+    choiseProdBtn.classList.remove("hover");
+    formOne.classList.remove("active");
+    formTwo.classList.remove("active");
 });
+
+
+var tabs = [choiseProdBtn, choiseInfBtn];
+var contents = [formOne, formTwo];
+tabs.forEach((tab, index) => {
+    tab.addEventListener('click', () => {
+        for (let i = 0; i < tabs.length; i++) {
+            tabs[i].classList.remove('hover');
+            contents[i].classList.remove('active');
+        }
+        tab.classList.add('hover');
+        contents[index].classList.add('active');
+    })
+});
+
+console.log(tabs[0]);
